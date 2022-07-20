f7768c45fda150c3f4f601a606652100a941ebd3
007cherukuri <007cherukuri@gmail.com>
2022-07-20 15:42:06 +0530

    commit committed

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import './App.css';
+// import { useEffect } from 'react';
+import { ContextHolder } from '@frontegg/rest-api';
+import { useAuth, useLoginWithRedirect } from "@frontegg/react";
+
+function App() {
+  const { user, isAuthenticated } = useAuth();
+  const loginWithRedirect = useLoginWithRedirect();
+
+  // Uncomment this to redirect to login automatically
+  // useEffect(() => {
+  //   if (!isAuthenticated) {
+  // loginWithRedirect();
+  //   }
+  // }, [isAuthenticated, loginWithRedirect]);
+
+  const logout = () => {
+    const baseUrl = ContextHolder.getContext().baseUrl;
+    window.location.href = `${baseUrl}/oauth/logout?post_logout_redirect_uri=${window.location}`;
+  };
+
+  return (
+    <div className="App">
+      { isAuthenticated ? (
+        <div>
+          <div>
+            <img src={user?.profilePictureUrl} alt={user?.name}/>
+          </div>
+          <div>
+            <span>Logged in as: {user?.name}</span>
+          </div>
+          <div>
+            <button onClick={() => alert(user.accessToken)}>What is my access token?</button>
+          </div>
+          <div>
+            <button onClick={() => logout()}>Click to logout</button>
+          </div>
+        </div>
+      ) : (
+        <div class="center">
+          <img src='https://qx-fleet-s-qa.azurewebsites.net/static/images/logo.png'></img>
+          <br></br>
+          <button onClick={() => loginWithRedirect()}>Click me to login</button>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom'; // For react 17
+// For react 18: import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+
+import { FronteggProvider } from '@frontegg/react';
+
+const contextOptions = {
+  baseUrl: 'https://app-pl6kejjbrfvm.frontegg.com',
+  clientId: 'c623c0c1-dd2a-4ee8-a46f-6c306918feba'
+};
+
+// For react 18: 
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+ReactDOM.render(
+    <FronteggProvider contextOptions={contextOptions} hostedLoginBox={true}>
+        <App />
+    </FronteggProvider>,
+    document.getElementById('root')
+);
